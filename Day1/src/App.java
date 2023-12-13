ae88879b04f02192252c9e741e1798c996f39ee3
Oracle.Exe <Oracle.ex3@gmail.com>
2023-12-13 07:36:10 -0500

    Pasted CodeHs code

--- Day1/src/App.java
+++ Day1/src/App.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
 public class App {
-    public static int calculateNumForLine(String lineText) {
-        return 45;
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    public static int calculateNumForLine(String input) {
+            int first_num_index = -1;
+            int last_num_index = -1;
+            for (int index = 0; index < input.length(); index++) {
+                String current_character = input.substring(index, index+1);
+                boolean foundNum = false;
+                if ("1".equals(current_character) ||
+                    "2".equals(current_character) ||
+                    "3".equals(current_character) ||
+                    "4".equals(current_character) ||
+                    "5".equals(current_character) ||
+                    "6".equals(current_character) ||
+                    "7".equals(current_character) ||
+                    "8".equals(current_character) ||
+                    "9".equals(current_character)
+                    ) {
+                    foundNum = true;
+                }
+                if (foundNum == true) {
+                    last_num_index = index;
+                    if (first_num_index == -1) {
+                       first_num_index = index;
+                    }
+                }
+            }
+    
+            System.out.printf("%c%c%n", input.charAt(first_num_index), input.charAt(last_num_index));
     }
     public static void main(String[] args) throws Exception {
         String lineText = "ab3093az";
@@ -8,3 +36,5 @@ public class App {
         System.out.println("Val for " + lineText + " is " + String.valueOf(valForLine));
     }
 }
+
+git config --global user.email "you"
